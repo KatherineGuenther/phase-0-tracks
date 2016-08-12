be2d11af62a893d1d41378d867b77f54fea940a5
Katherine Guenther <guenther@peak.org>
2016-08-12 15:48:00 -0700

    Add code to handle case

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,42 +1,47 @@
-def get_next(letter, str)
+def next_in_string(letter, str)
 	#For a letter in a given string, return the next letter in the string or wrap around if letter is last in string.
-	#Case sensitive
+	#Handle case
 
-	if letter == str[-1]
-		str[0]
+	if letter.downcase == str[-1]
+		new_letter = str[0]
 	else
-		idx = str.index(letter)
-		str[idx + 1]
+		new_letter = str[str.index(letter.downcase) + 1]
+	end
+
+	if letter == letter.downcase
+		new_letter
+	else
+		new_letter.upcase
 	end
 end
 
 
-def fake_name(name)
+def make_alias(name)
 	#Define variables vowels = "aeiou" and consonants = "bcdf..."
 	vowels = "aeiou"
 	consonants = "bcdfghjklmnpqrstvwxyz"
-	result = ""
+	fake_name = ""
 
 	#Swap first and last names.  Convert string to array of words, then reverse and join.
 	names = name.split(" ")
 	names.reverse!
 	name = names.join(" ")
 
-	#Change any vowel to the next vowel and any consonant to the next consonant.
+	#Change any vowel to the next vowel and any consonant to the next consonant. Leave spaces alone.
 	#Step through string using #each_char and test each letter for inclusion in vowels or consonants.
-	#If vowel, change letter to next in vowels string. If consonant, change letter to next in consonant string.  Handle case.
+	#If vowel, change letter to next in vowels string. If consonant, change letter to next in consonant string.
 	name. each_char {|letter|
 		if vowels.include?(letter.downcase)
-			result += get_next(letter.downcase, vowels)	
+			fake_name += next_in_string(letter, vowels)	
 		elsif consonants.include?(letter.downcase)
-			result += get_next(letter.downcase, consonants)
+			fake_name += next_in_string(letter, consonants)
 		else
-			result += letter
+			fake_name += letter
 		end
 	}
 
-	result
+	fake_name
 end
 
-p fake_name("Felicia Torres")
-p fake_name("Zoro Smith")
+p make_alias("Felicia Torres")
+p make_alias("Zoro Smith")
