01bf990605a53683357caf46e35e4767e9935688
Katherine Guenther <guenther@peak.org>
2016-08-12 13:50:40 -0700

    Psuedocode Release 0

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,8 @@
+#Swap first and last names.  Convert string to array of words, then reverse and join.
+
+#Change any vowel to the next vowel and any consonant to the next consonant.
+#Define variables vowels = "aeiou" and consonants = "bcdf..."
+
+#Step through string using #each_char and test each letter for inclusion in vowels or consonants
+
+#If vowel, change letter to next in vowels string. If consonant, change letter to next in consonant string.  Handle case and edge cases.
