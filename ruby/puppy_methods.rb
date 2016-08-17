255ea5a25b2d0d5fb31e4e0400b3a0d1cefebb7a
Katherine Guenther <guenther@peak.org>
2016-08-17 14:45:49 -0700

    Finish Release 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -3,7 +3,7 @@ class Puppy
   def initialize
   	puts "Initializing new puppy instance..."
   end
-  
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -26,9 +26,36 @@ class Puppy
   end
 end
 
+class Programmer
+
+  def initialize
+  	puts "Initializing new programmer instance..."
+  end
+
+  def pull_hair_out
+  	puts "Argghh!"
+  end
+
+  def hours_in_front_of_computer(hours)
+  	puts "I spent #{hours} at the computer this week!"
+  end
+end
+
 Max = Puppy.new
 Max.fetch("ball")
 Max.speak(5)
 Max.roll_over
 puts Max.dog_years(5)
 Max.poop
+
+Fernando = Programmer.new
+Fernando.pull_hair_out
+Fernando.hours_in_front_of_computer(40)
+
+programmers = []
+50.times {programmers << Programmer.new}
+programmers.each do | poor_guy|
+	poor_guy.pull_hair_out
+	poor_guy.hours_in_front_of_computer(40)
+end
+
