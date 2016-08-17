d90b6392cba706517dc12c5ee3f45e544c7b4cda
Katherine Guenther <guenther@peak.org>
2016-08-17 14:15:17 -0700

    Finish Release 1

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,34 @@
 class Puppy
 
+  def initialize
+  	puts "Initializing new puppy instance..."
+  end
+  
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
+  def speak(num)
+  	num.times {puts "Woof"}
+  end
+
+  def roll_over
+  	puts "rolls over"
+  end
+
+  def dog_years(years)
+  	years*7
+  end
+
+  def poop
+  	puts "Let me out!"
+  end
 end
 
+Max = Puppy.new
+Max.fetch("ball")
+Max.speak(5)
+Max.roll_over
+puts Max.dog_years(5)
+Max.poop
