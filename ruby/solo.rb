3ae4269646ee55b618d23c2a8364523713c8bda9
Katherine Guenther <guenther@peak.org>
2016-08-19 13:56:41 -0700

    Write LibraryBook class

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,6 +1,39 @@
-# Define class Library_Book
-# Add attributes for title, due date, read yet and rating
-# Add method to initialize new instance with title and due date
-# Add methods to get title and due date
-# Add method to get and set read yet and rating
-# Add method to determine if overdue
+# Define class LibraryBook
+class LibraryBook
+
+  # Add method to initialize new instance with title and due date
+  # Add attributes for read yet and rating
+  def initialize(title, days_until_due)
+    @title = title
+    @due_date = (Time.now + (60 * 60 * 24 * days_until_due))
+    read_yet = false
+    rating = nil
+  end
+
+  # Add methods to get title and due date
+  attr_reader :title
+  attr_reader :due_date
+
+  # Add method to get and set read yet and rating
+  attr_accessor :read_yet
+  attr_accessor :rating
+
+  # Add method to determine if overdue
+  def overdue?
+    Time.now > @due_date
+  end
+
+end
+
+
+
+
+book1 = LibraryBook.new("Learn to Program", 3)
+p book1
+p book1.title
+p book1.due_date
+
+book1.read_yet = true
+book1.rating = "***"
+p book1.overdue?
+p book1
