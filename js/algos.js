18151634b21d7c98e4cb030c9421b74fb5172f6e
Katherine Guenther <guenther@peak.org>
2016-08-26 13:56:22 -0700

    Add Key-Value match

--- js/algos.js
+++ js/algos.js
@@ -4,7 +4,7 @@
 // If the current string is longer than current longest, update current longest
 // Return current longest
 
-function longest_string(strings) {
+function longestString(strings) {
 
   var longest = "";
 
@@ -17,5 +17,29 @@ function longest_string(strings) {
   return longest;
 }
 
-console.log(longest_string(["long phrase","longest phrase","longer phrase"]));
-console.log(longest_string(["abcdef","abc","a"]));
+// Write a function that takes to objects and returns true if the objects share
+// at least one key-value pair.  Otherwise return false.
+// Iterate through the properties of the first object and look for an equivalent property in the second object.
+// If a match is found, compare the values.  If the values are equal, return.  Otherwise return false.
+
+function keyValueMatch(object1, object2){
+
+  for (var property in object1) {
+    if (object1[property] == object2[property]) {
+      return true;
+    }
+  }
+
+  return false
+}
+
+console.log(longestString(["long phrase","longest phrase","longer phrase"]));
+console.log(longestString(["abcdef","abc","a"]));
+
+var dog = {name: "Fido", age: 4, color: "black"};
+var cat = {name: "Fluffy", age: 4, gender: "male"};
+var car = {make: "Toyota", model: "Prius", color: "black"};
+
+console.log(keyValueMatch(dog, cat));
+console.log(keyValueMatch(car, cat));
+console.log(keyValueMatch(dog, car));
